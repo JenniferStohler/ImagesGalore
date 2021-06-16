26ad1ad98d62f76725904aacd343d4fd30555c5a
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-06-15 20:09:08 -0600

    new app stuff

--- ImagesGalore-client/src/env.js
+++ ImagesGalore-client/src/env.js
@@ -1,5 +1,5 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = '{{AUTH DOMAIN}}'
+export const audience = '{{AUTH AUDIENCE}}'
+export const clientId = '{{AUTH CLIENT ID}}'
+
