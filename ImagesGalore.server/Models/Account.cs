f5d96fc84154913366c0b416d3c2ea5fd17d123a
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-06-16 17:10:59 -0600

    server setup

--- ImagesGalore.server/Models/Account.cs
+++ ImagesGalore.server/Models/Account.cs
@@ -1,10 +1,11 @@
 namespace ImagesGalore.server.Models
 {
-  public class Boards
+  public class Account
   {
     public int Id { get; set; }
     public string CreatorId { get; set; }
-
+    public string Email { get; set; }
+    public string Name { get; set; }
     public string Image { get; set; }
   }
 }
