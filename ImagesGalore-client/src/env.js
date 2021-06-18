ae9950ed45fafa536c5c0d9524e8dcf4ad7f0b1a
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-06-17 19:13:34 -0600

    env.js done

--- ImagesGalore-client/src/env.js
+++ ImagesGalore-client/src/env.js
@@ -1,5 +1,6 @@
-export const baseURL = dev ? 'https://localhost:5001' : ''
-export const domain = '{{AUTH DOMAIN}}'
-export const audience = '{{AUTH AUDIENCE}}'
-export const clientId = '{{AUTH CLIENT ID}}'
 
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-tkj14bxx.us.auth0.com'
+export const audience = 'https://ImagesGalore.com'
+export const clientId = 'ANLaiwl0fjALvS9Pf7YybpiXRAEBILXM'
